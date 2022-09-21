be638cef206060b19356faff2421df3e713a5a60
Szymon Kamiński <szy.kaminski@gmail.com>
2022-09-21 15:18:13 +0200

    modified: filled with basic content

---
+++ src/components/TheContainer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <span>Liczba zadań: </span> 
+        <span>NULL</span> <!-- TODO: count list elements from list component -->
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+  export default class TheContainer extends Vue {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
