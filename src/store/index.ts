daaacf7e9f0576d26f038a01661d615d2b2ce334
Szymon Kamiński <szy.kaminski@gmail.com>
2022-09-21 16:05:04 +0200

    import task interface to vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { Task } from '../types'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tasks: [] as Task[]
   },
   getters: {
   },
