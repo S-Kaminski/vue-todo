e0aef1c17edaf2daf65a07109407ef6d8e5876f3
Szymon Kamiński <szy.kaminski@gmail.com>
2022-09-22 14:23:19 +0200

    added button (task remover) and scoped css styling, removed from displaying boolean importance status

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -2,8 +2,8 @@
     <div>
         <li v-for="task in $store.state.tasks" :key="task.id" :class="{'important': task.important}">
             <input type="checkbox"/>
-            <span>{{ task.description }} => </span>
-            <span>{{ task.important }}</span>
+            <span>{{ task.description }}</span>
+            <button class="remover">x</button>
         </li>
         
     </div>
@@ -21,5 +21,16 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 <style scoped>
 li.important{
     color:red;
+}
+button.remover {
+    border: none;
+    background-color: inherit;
+    padding: 6px 10px;
+    font-size: 16px;
+    cursor: pointer;
+    display: inline-block;
+}
+button.remover:hover{
+    background: #eee;
 }
 </style>
