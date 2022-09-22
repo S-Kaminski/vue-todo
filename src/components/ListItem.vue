8459bab7e6ac9856c88bcc44405b73cda8e27c8e
Szymon Kamiński <szy.kaminski@gmail.com>
2022-09-22 11:31:04 +0200

    create component ListItem

---
+++ src/components/ListItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+  export default class ListItem extends Vue {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
