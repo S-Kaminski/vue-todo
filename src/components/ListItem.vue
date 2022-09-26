4d0053698c7019eb940a5daffd88c72026f20769
Szymon Kamiński <szy.kaminski@gmail.com>
2022-09-26 15:04:44 +0200

    modified: added temporary commented code to work with later

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,18 +1,20 @@
 <template>
     <div>
         <li v-for="(task, index) in $store.state.tasks" :key="task.id" :class="{'important': task.important}">
-            <input type="checkbox" :value="checkedTask(index,task.id)" v-model="checkedTasks" @change="sortSelectedTasks"/>
+            <input type="checkbox" :value="task.id" v-model="checkedTasks" @change="sortSelectedTasks"/>
             <span> {{ (index+1)}}. {{ task.description }}</span>
             <button class="remover" @click="removeTask(task.id)">x</button>
         </li>
         <TheFooter @move-up="moveUp" @move-down="moveDown" @mark-important="markImportant" @remove-all-tasks="removeAllTasks"></TheFooter>
-        {{checkedTasks}}
+        {{checkedTasks}} <br>{{$store.state.tasks}}
+        <!--{{consecutiveRanges(checkedIndexes)}} !-->
+        
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
-import { Direction, Task, SelectedTask } from '../types'
+import { Direction, Task } from '../types'
 import TheFooter from './TheFooter.vue'
 
 @Component({
@@ -22,16 +24,11 @@ import TheFooter from './TheFooter.vue'
 })
   export default class ListItem extends Vue {
     //Data
+    
     checkedTasks:  [] = [] ;
+    //checkedIndexes: number[] = [];
 
     //Methods
-    public checkedTask(i: number, id: string){
-        return {
-            index: i,
-            id: id
-        }
-    }
-
     public removeTask(taskId: string): void {
         if(confirm("Czy na pewno chcesz usunąć to zadanie?")){
             const index = this.$store.state.tasks.findIndex( (task: Task) => task.id === taskId);
@@ -39,17 +36,50 @@ import TheFooter from './TheFooter.vue'
         }
     }
 
-    public sortSelectedTasks(){
-        this.checkedTasks.sort((a:SelectedTask,b:SelectedTask) =>{
-            return a.index - b.index;
+    public sortSelectedTasks() {
+        /** this.checkedIndexes.splice(0);
+        this.checkedTasks.forEach( (task) => {
+            this.checkedIndexes.push(this.$store.state.tasks.findIndex((t: Task) => t.id == task))
         })
-        console.log("Updated order of selected tasks.");
+        this.checkedTasks.sort();
+        console.log("Updated order of selected tasks."); */
     }
+    /**public consecutiveRanges(array) {
+        let length = 1;
+
+        if (array.length == 0) console.log("consecutiveRanges: return1");
+        for (let index = 1; index < array.length; index++) {
+            
+            if (length == 1 || array[index] - array[index-1] != 1) {
+                if(length == 1)
+                    console.log("consecutiveRanges: condt1 " + array[index - length]);
+                else 
+                console.log("consecutiveRanges: condt2 " + array[index-length] + "->" + array[index - 1])
+                length = 1;
+            }
+            else length++;
+        }
+    } */
 
     //Watch
-    @Watch("movementUp")
+    @Watch("movementUp") //TODO content
     public moveUp(value: Direction): void { 
+        const checkedTasksIndexes: number[] = [];
+        this.checkedTasks.forEach( (task) => {
+            checkedTasksIndexes.push(this.$store.state.tasks.findIndex((t: Task) => t.id == task))
+        })
+        checkedTasksIndexes.sort();
 
+        for(let i = 0; i < checkedTasksIndexes.length; i++){
+            if(checkedTasksIndexes[i]>0)
+            {
+                const newPosition = checkedTasksIndexes[i]-1;
+                const arrayElement = this.$store.state.tasks.splice(checkedTasksIndexes[i],1)[0];
+                this.$store.state.tasks.splice(newPosition,0,arrayElement);
+
+                checkedTasksIndexes[i] = -1;
+            }
+        }
         console.log(value);
     }
 
@@ -57,7 +87,7 @@ import TheFooter from './TheFooter.vue'
     public moveDown(value: Direction): void {
         console.log(value);
     }
-    
+
     @Watch("changeImportance")
     public markImportant(): void {
         
