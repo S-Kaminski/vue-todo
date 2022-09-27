13e640b076610da5c67f911726c5b848126181d4
Szymon Kamiński <szy.kaminski@gmail.com>
2022-09-27 14:09:47 +0200

    modified: added watchers to oberve status change on checkbox selection and emiters to notify parent (TheContainer)

--- src/components/SingleItem.vue
+++ src/components/SingleItem.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
         <li> 
-            <input type="checkbox"/>
+            <input type="checkbox" :value="task.id" v-model="checkboxStatus"/>
             {{index}} . {{task.description}} 
             <button class="remover" @click="removeTask(task.id)">x</button>
         </li>
@@ -10,7 +10,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator';
+import { Component, Vue, Prop, Emit, Watch } from 'vue-property-decorator';
 import { Task } from '../types'
     @Component
     export default class SingleItem extends Vue {
@@ -18,15 +18,32 @@ import { Task } from '../types'
         @Prop() readonly index!: number;
         @Prop() readonly task!: Task;
         //Data
+        checkboxStatus = false;
+        //...
         //Methods
         public removeTask(id: string): void {
-        if(confirm("Czy na pewno chcesz usunąć to zadanie?")) {
-            const index = this.$store.state.tasks.findIndex( (task: Task) => task.id === id );
-            this.$store.state.tasks.splice(index,1);
+            if(confirm("Czy na pewno chcesz usunąć to zadanie?")) {
+                const index = this.$store.state.tasks.findIndex( (task: Task) => task.id === id );
+                this.$store.state.tasks.splice(index,1);
+            }
         }
-    }
+        //Emit
+        @Emit("task-selection")
+        taskSelection(status: boolean): { taskId: string, taskStatus: boolean } {
+            let task = this.task.id;
+            console.log("SingleItem component Emit -> selectedTask " + status + " <-");
+            return { taskId: task, taskStatus: status};
+        }
         //Watch
+        @Watch("checkboxStatus", {immediate: true})
+        public clickCheckbox(): void {
+            console.log("SingleItem component Watch -> selectionStatus changed");
+                this.taskSelection(this.checkboxStatus);
+        }
+        //...
+        
     }
+        
 </script>
 
 <style scoped>
