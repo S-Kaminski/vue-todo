38b4200febeb6193fd01cacbcbf3e641fbae7a72
Szymon Kamiński <szy.kaminski@gmail.com>
2022-09-27 14:08:33 +0200

    modified: added watcher to observe changes in selected tasks

--- src/components/TheContainer.vue
+++ src/components/TheContainer.vue
@@ -9,14 +9,15 @@
         <input type="checkbox" v-model="taskImportance" />
         <label>Ważne</label>
       </span>      
-      <SingleItem v-for="(task, index) in $store.state.tasks" :key="task.id + index" :index="index" :task="task"></SingleItem>
+      <SingleItem v-for="(task, index) in $store.state.tasks" :key="task.id + index" :index="index" :task="task" @task-selection="taskSelection"></SingleItem>
       <TheFooter></TheFooter>
       <!-- <ListItem></ListItem>-->
+      {{selectedTasks}}
     </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import { v4 as uuidv4 } from 'uuid';
 import ListItem from './ListItem.vue'
 import SingleItem from './SingleItem.vue'
@@ -36,6 +37,7 @@ import TheFooter from './TheFooter.vue'
     //Data
     taskDescription = "";
     taskImportance = false;
+    selectedTasks: string[] = [];
     //Methods
     public addNewTask(): void {
       if(this.taskDescription.length > 0)
@@ -47,7 +49,16 @@ import TheFooter from './TheFooter.vue'
         });
         this.taskDescription = "";
       }
-    } 
+    }
+    //Emit
+    //...
+    //Watch
+    @Watch("taskSelected")
+    taskSelection(value: { taskId: string, taskStatus: boolean }): void {
+      console.log("TheContainer received: { id: " + value.taskId + ", status: " + + value.taskStatus + "}");
+      if(value.taskStatus) this.selectedTasks.push(value.taskId);
+      else this.selectedTasks.splice(this.selectedTasks.findIndex( (task: string) => task === value.taskId),1 );
+    }
   }
 </script>
 
