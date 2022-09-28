aa9669de4218de13501fc4cd58555b03462e1346
Szymon Kamiński <szy.kaminski@gmail.com>
2022-09-28 10:22:48 +0200

    modified: little changes, refactorization

--- src/components/SingleItem.vue
+++ src/components/SingleItem.vue
@@ -1,10 +1,10 @@
 
 <template>
     <div>
-        <li> 
+        <li :class="{'important': task.important}"> 
             <input type="checkbox" :value="task.id" v-model="checkboxStatus"/>
             {{index}} . {{task.description}} 
-            <button class="remover" @click="removeTask(task.id)">x</button>
+            <button class="remover" @click.prevent="removeTask">x</button>
         </li>
     </div>
 </template>
@@ -19,29 +19,28 @@ import { Task } from '../types'
         @Prop() readonly task!: Task;
         //Data
         checkboxStatus = false;
-        //...
         //Methods
-        public removeTask(id: string): void {
+        public removeTask(): void {
             if(confirm("Czy na pewno chcesz usunąć to zadanie?")) {
-                const index = this.$store.state.tasks.findIndex( (task: Task) => task.id === id );
+                if(this.checkboxStatus) this.taskSelection(false);
+                const index = this.$store.state.tasks.findIndex( (task: Task) => task.id === this.task.id );
                 this.$store.state.tasks.splice(index,1);
             }
         }
         //Emit
         @Emit("task-selection")
         taskSelection(status: boolean): { taskId: string, taskStatus: boolean } {
-            let task = this.task.id;
             console.log("SingleItem component Emit -> selectedTask " + status + " <-");
-            return { taskId: task, taskStatus: status};
+            return { taskId: this.task.id, taskStatus: status};
         }
         //Watch
-        @Watch("checkboxStatus", {immediate: true})
+        @Watch("checkboxStatus")
         public clickCheckbox(): void {
             console.log("SingleItem component Watch -> selectionStatus changed");
                 this.taskSelection(this.checkboxStatus);
         }
-        //...
         
+        //...
     }
         
 </script>
